5979c5fb11084369fab511cd74091776d05b8d0e
Nideesh Kumar <nideesharumugam2006@gmail.com>
2026-03-25 14:36:47 +0530

    Replaced old project with new project

--- server/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { trackPageView, getAnalytics } = require('../controllers/analyticsController');
-
-router.post('/pageview', trackPageView);    // POST /api/analytics/pageview
-router.get('/', getAnalytics);              // GET  /api/analytics
-
-module.exports = router;
--- server/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createContact, getContacts } = require('../controllers/contactController');
-
-router.post('/', createContact);    // POST /api/contacts
-router.get('/', getContacts);       // GET  /api/contacts
-
-module.exports = router;
